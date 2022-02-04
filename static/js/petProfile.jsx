a6988e8e0aba45554d759d35e5ba1e0c91c7477f
Jennifer Lei <jenniferlei1928@gmail.com>
2022-02-04 01:28:58 +0000

    Changed Add Pet function to a React component

--- static/js/petProfile.jsx
+++ static/js/petProfile.jsx
@@ -69,15 +69,12 @@ function CheckIns(props) {
 
 function PetProfile(props) {
   const id_name = `pet-${props.pet_id}`;
-  const collapse_href = `#collapse-view-check-ins-${props.pet_id}`;
-  const collapse_id = `collapse-view-check-ins-${props.pet_id}`;
+  const modal_edit_target = `#modal-edit-${props.pet_id}`;
+  const modal_edit_id = `modal-edit-${props.pet_id}`;
+  const modal_edit_form_id = `modal-edit-form-${props.pet_id}`;
 
   return (
-    <div
-      className="pet-profile row card card-body mb-3"
-      key={id_name}
-      id={id_name}
-    >
+    <div className="pet-profile row card card-body mb-3" id={id_name}>
       <h4>{props.pet_name}</h4>
 
       {props.pet_imgURL !== null && (
@@ -102,21 +99,13 @@ function PetProfile(props) {
 
       <div className="row">
         <div className="col">
-          <form
-            action="/edit-pet"
-            className="edit-form"
-            id="form-edit-pet"
-            method="POST"
+          <button
+            className="btn btn-link btn-sm"
+            data-bs-toggle="modal"
+            data-bs-target={modal_edit_target}
           >
-            <button
-              className="btn btn-link btn-sm"
-              type="submit"
-              name="edit"
-              value={props.pet_id}
-            >
-              <small>Edit</small>
-            </button>
-          </form>
+            <small>Edit</small>
+          </button>
         </div>
         <div className="col">
           <form
@@ -136,76 +125,350 @@ function PetProfile(props) {
           </form>
         </div>
       </div>
-      <div className="collapse card card-body" id={collapse_id}>
-        stuff
+      <div
+        class="modal fade"
+        id={modal_edit_id}
+        tabindex="-1"
+        aria-labelledby={modal_edit_form_id}
+        aria-hidden="true"
+      >
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id={modal_edit_form_id}>
+                Edit Pet Info
+              </h5>
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div class="modal-body">
+              {/* ModalFormContainer goes here */}
+
+              <div class="modal-footer">
+                <button class="btn btn-sm btn-primary btn-block" type="submit">
+                  Save
+                </button>
+
+                <button
+                  type="button"
+                  class="btn btn-sm btn-secondary btn-block"
+                  data-bs-dismiss="modal"
+                >
+                  Close
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
 }
 
-// function AddPetProfile(props) {
-//   const [pet_name, setPetName] = React.useState('');
-//   const [gender, setGender] = React.useState('');
-//   const [birthday, setBirthday] = React.useState('');
-//   const [breed, setBreed] = React.useState('');
-//
-//   function addNewProfile() {
-//     fetch('/add-pet', {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json',
-//       },
-//       body: JSON.stringify({pet_name, gender, birthday, breed}),
-//     }).then(response => {
-//       response.json().then(jsonResponse => {
-//         const {petAdded} = jsonResponse; // same as petAdded = jsonResponse.petAdded
-//         const {pet_id, user_id, pet_name, gender, birthday, breed, pet_imgURL, img_public_id} = petAdded;
-//         props.addProfile(pet_id, pet_name, gender, birthday, breed);
-//       });
-//     });
-//   }
-//
-//   return (
-//     <React.Fragment>
-//       <h2>Add New Pet</h2>
-//       <label htmlFor="petNameInput">
-//         Name
-//         <input
-//           name="pet_name"
-//           value={pet_name}
-//           onChange={event => setPetName(event.target.value)}
-//           id="petNameInput"
-//           style={{marginLeft: '5px'}}
-//         />
-//       </label>
-//       <label htmlFor="genderInput" style={{marginLeft: '10px', marginRight: '5px'}}>
-//         Gender
-//         <input name="gender" value={gender} onChange={event => setGender(event.target.value)} id="genderInput" />
-//       </label>
-//       <label htmlFor="birthdayInput" style={{marginLeft: '10px', marginRight: '5px'}}>
-//         Birthday
-//         <input name="birthday" value={birthday} onChange={event => setBirthday(event.target.value)} id="birthdayInput" />
-//       </label>
-//       <label htmlFor="breedInput" style={{marginLeft: '10px', marginRight: '5px'}}>
-//         Breed
-//         <input name="breed" value={breed} onChange={event => setBreed(event.target.value)} id="breedInput" />
-//       </label>
-//       <label htmlFor="imgFileInput" style={{marginLeft: '10px', marginRight: '5px'}}>
-//         Breed
-//         <input type="file" name="breed" value={breed} onChange={event => setBreed(event.target.value)} id="breedInput" />
-//       </label>
-//       <button type="button" style={{marginLeft: '10px'}} onClick={addNewProfile}>
-//         Add
-//       </button>
-//     </React.Fragment>
-//   );
-// }
+function EditPetProfile(props) {
+  const [pet_name, setPetName] = React.useState("");
+  const [gender, setGender] = React.useState("");
+  const [birthday, setBirthday] = React.useState("");
+  const [breed, setBreed] = React.useState("");
+  const [my_file, setMyFile] = React.useState("");
+
+  function EditProfile() {
+    fetch("/edit-pet", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ pet_name, gender, birthday, breed, pet_imgURL }),
+    }).then((response) => {
+      response.json().then((jsonResponse) => {
+        const { petAdded } = jsonResponse; // same as petAdded = jsonResponse.petAdded
+        const { pet_id, pet_name, gender, birthday, breed, pet_imgURL } =
+          petAdded;
+        props.addProfile(pet_id, pet_name, gender, birthday, breed, pet_imgURL);
+      });
+    });
+  }
+
+  return (
+    <React.Fragment>
+      <div
+        className="modal fade"
+        id="modal-add-pet"
+        tabindex="-1"
+        aria-labelledby="modal-add-pet-label"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="modal-add-pet-label">
+                Add Pet Info
+              </h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+              <form
+                action="/add-pet"
+                id="form-add-pet"
+                method="POST"
+                enctype="multipart/form-data"
+              >
+                <div className="mb-3">
+                  <label htmlFor="pet_name" className="sr-only">
+                    Name
+                  </label>
+                  <input
+                    type="text"
+                    name="pet_name"
+                    value={pet_name}
+                    onChange={(event) => setPetName(event.target.value)}
+                    className="form-control input-lg"
+                    placeholder="Name"
+                    required
+                  />
+                </div>
+
+                <div className="mb-3">
+                  <label for="gender" className="sr-only">
+                    Gender
+                  </label>
+                  <select
+                    name="gender"
+                    value={gender}
+                    onChange={(event) => setGender(event.target.value)}
+                    className="form-control input-lg"
+                  >
+                    <option value=""></option>
+                    <option value="female">female</option>
+                    <option value="male">male</option>
+                  </select>
+                </div>
+
+                <div className="mb-3">
+                  <label htmlFor="birthday" className="sr-only">
+                    Birthday
+                  </label>
+                  <input
+                    type="date"
+                    name="birthday"
+                    value={birthday}
+                    onChange={(event) => setBirthday(event.target.value)}
+                    className="form-control input-lg"
+                  />
+                </div>
+
+                <div className="mb-3">
+                  <label htmlFor="breed" className="sr-only">
+                    Breed
+                  </label>
+                  <input
+                    type="text"
+                    name="breed"
+                    value={breed}
+                    onChange={(event) => setBreed(event.target.value)}
+                    className="form-control input-lg"
+                    placeholder="Breed"
+                  />
+                </div>
+
+                <div className="mb-3">
+                  <label htmlFor="my_file" className="sr-only">
+                    Image
+                  </label>
+                  <input
+                    type="file"
+                    name="my_file"
+                    className="form-control input-lg"
+                    value={my_file}
+                    onChange={(event) => setMyFile(event.target.value)}
+                    accept="image/png, image/jpeg"
+                  />
+                </div>
+                <div className="modal-footer">
+                  <button
+                    className="btn btn-sm btn-primary btn-block mt-4"
+                    type="submit"
+                  >
+                    Save
+                  </button>
+                  <button
+                    type="button"
+                    className="btn btn-sm btn-secondary btn-block mt-4"
+                    data-bs-dismiss="modal"
+                  >
+                    Close
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+}
+
+function AddPetProfile(props) {
+  const [pet_name, setPetName] = React.useState(props.pet_name);
+  const [gender, setGender] = React.useState(props.gender);
+  const [birthday, setBirthday] = React.useState(props.birthday);
+  const [breed, setBreed] = React.useState(props.breed);
+  const [my_file, setMyFile] = React.useState(props.my_file);
+
+  function addNewProfile() {
+    fetch("/add-pet", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ pet_name, gender, birthday, breed, pet_imgURL }),
+    }).then((response) => {
+      response.json().then((jsonResponse) => {
+        const { petAdded } = jsonResponse; // same as petAdded = jsonResponse.petAdded
+        const { pet_id, pet_name, gender, birthday, breed, pet_imgURL } =
+          petAdded;
+        props.addProfile(pet_id, pet_name, gender, birthday, breed, pet_imgURL);
+      });
+    });
+  }
+
+  return (
+    <React.Fragment>
+      <div
+        className="modal fade"
+        id="modal-add-pet"
+        tabindex="-1"
+        aria-labelledby="modal-add-pet-label"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="modal-add-pet-label">
+                Add Pet Info
+              </h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+              <form
+                action="/add-pet"
+                id="form-add-pet"
+                method="POST"
+                enctype="multipart/form-data"
+              >
+                <div className="mb-3">
+                  <label htmlFor="pet_name" className="sr-only">
+                    Name
+                  </label>
+                  <input
+                    type="text"
+                    name="pet_name"
+                    value={pet_name}
+                    onChange={(event) => setPetName(event.target.value)}
+                    className="form-control input-lg"
+                    placeholder="Name"
+                    required
+                  />
+                </div>
+
+                <div className="mb-3">
+                  <label for="gender" className="sr-only">
+                    Gender
+                  </label>
+                  <select
+                    name="gender"
+                    value={gender}
+                    onChange={(event) => setGender(event.target.value)}
+                    className="form-control input-lg"
+                  >
+                    <option value=""></option>
+                    <option value="female">female</option>
+                    <option value="male">male</option>
+                  </select>
+                </div>
+
+                <div className="mb-3">
+                  <label htmlFor="birthday" className="sr-only">
+                    Birthday
+                  </label>
+                  <input
+                    type="date"
+                    name="birthday"
+                    value={birthday}
+                    onChange={(event) => setBirthday(event.target.value)}
+                    className="form-control input-lg"
+                  />
+                </div>
+
+                <div className="mb-3">
+                  <label htmlFor="breed" className="sr-only">
+                    Breed
+                  </label>
+                  <input
+                    type="text"
+                    name="breed"
+                    value={breed}
+                    onChange={(event) => setBreed(event.target.value)}
+                    className="form-control input-lg"
+                    placeholder="Breed"
+                  />
+                </div>
+
+                <div className="mb-3">
+                  <label htmlFor="my_file" className="sr-only">
+                    Image
+                  </label>
+                  <input
+                    type="file"
+                    name="my_file"
+                    className="form-control input-lg"
+                    value={my_file}
+                    onChange={(event) => setMyFile(event.target.value)}
+                    accept="image/png, image/jpeg"
+                  />
+                </div>
+                <div className="modal-footer">
+                  <button
+                    className="btn btn-sm btn-primary btn-block mt-4"
+                    type="submit"
+                  >
+                    Save
+                  </button>
+                  <button
+                    type="button"
+                    className="btn btn-sm btn-secondary btn-block mt-4"
+                    data-bs-dismiss="modal"
+                  >
+                    Close
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+}
 
 function PetProfileContainer() {
   const [pets, setPets] = React.useState([]);
 
-  function addProfile(pet_id, pet_name, gender, birthday, breed) {
-    const pet_imgURL = "static/img/placeholder.png";
+  function addProfile(pet_id, pet_name, gender, birthday, breed, pet_imgURL) {
     const newPet = { pet_id, pet_name, gender, birthday, breed, pet_imgURL }; // equivalent to { cardId: cardId, skill: skill, name: name, imgUrl: imgUrl }
     const currentPets = [...pets]; // makes a copy of pets. similar to doing currentPets = pets[:] in Python
     // [...currentPets, newPet] is an array containing all elements in currentPets followed by newPet
@@ -223,6 +486,7 @@ function PetProfileContainer() {
   for (const currentPet of pets) {
     petProfiles.push(
       <PetProfile
+        key={currentPet.pet_id}
         pet_id={currentPet.pet_id}
         pet_name={currentPet.pet_name}
         gender={currentPet.gender}
@@ -235,8 +499,23 @@ function PetProfileContainer() {
 
   return (
     <React.Fragment>
-      {/* <AddPetProfile addProfile={addProfile} /> */}
-      {/* <h2>Pet Profiles</h2> */}
+      <div className="row">
+        <div className="col">
+          <h2>Pets</h2>
+        </div>
+
+        <div className="col">
+          <button
+            type="button"
+            className="btn btn-link btn-sm"
+            data-bs-toggle="modal"
+            data-bs-target="#modal-add-pet"
+          >
+            <small>+ Pet</small>
+          </button>
+        </div>
+      </div>
+      <AddPetProfile addProfile={addProfile} />
       <div className="grid">{petProfiles}</div>
     </React.Fragment>
   );
